d7f5ad566600116fa9f98b4a7b411fabdaf87a0b
bb1 <birk.brauer@gmail.com>
2022-11-06 19:58:29 +0000

    added custom colors

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/styles/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -55,4 +55,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  buefy: {
+    css: false,
+    materialDesignIcons: false,
+  },
 }
