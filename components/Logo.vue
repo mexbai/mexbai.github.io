7a473109cb30fd89629738ffc6efabfaa3ed8da9
bb1 <birk.brauer@gmail.com>
2022-11-07 20:03:42 +0000

    fixed logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="column mexbai-logo">
-    <div class="mexb">mexb</div>
-    <div class="ai">ai</div>
+  <div class="columns mexbai-logo">
+    <div class="column mexb">mexb</div>
+    <div class="column ai">ai</div>
   </div>
 </template>
 
@@ -22,13 +22,28 @@ export default {
 <style scoped>
 .mexbai-logo {
   font-family: 'Exo2', sans-serif;
+  padding: 0.25em;
 }
 
 .mexb,
 .ai {
   border: 0.1em solid;
-  height: 1.1em;
-  padding: 0 1rem;
+  padding: 0 0.25em;
   transition: all 150ms linear;
+}
+
+.ai {
+  border-left: 0;
+  border-color: transparent;
+}
+
+.mexbai-logo:hover .mexb {
+  border-color: #fff;
+  background-color: #fff;
+  color: #3b5c61;
+}
+
+.mexbai-logo:hover .ai {
+  border-color: unset;
 }
 </style>
